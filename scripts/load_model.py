c42acd89c029f9b33e19c040bfdc2a1fc3db40c7
dylandru <158249525+dylandru@users.noreply.github.com>
2024-09-04 06:49:50 -0400

    Update load_model function and Notebooks

--- scripts/load_model.py
+++ scripts/load_model.py
@@ -3,7 +3,7 @@ import os
 from tqdm import tqdm
 from function_utils.utils import model_aliases
 
-def load_model(model_alias: str, model_type = 'YOLO') -> None:
+def load_model(model_alias: str, model_type = 'YOLO') -> str:
     '''
     Loads a given baseball computer vision model into the repository.
 
@@ -11,7 +11,7 @@ def load_model(model_alias: str, model_type = 'YOLO') -> None:
         model_alias (str): Alias of the model to load that corresponds to a model file to download
 
     Returns:
-        None: Model is loaded into the given model weights directory.
+        model_weights_path (str): Path to where the model weights are saved within the repo.
     '''
 
     if model_type == 'YOLO':
@@ -23,27 +23,28 @@ def load_model(model_alias: str, model_type = 'YOLO') -> None:
         with open(model_txt_path, 'r') as file:
             link = file.read().strip()
 
-        filename = f"{os.path.dirname(model_txt_path)}/{os.path.splitext(os.path.basename(model_txt_path))[0]}.pt"
+        model_weights_path = f"{os.path.dirname(model_txt_path)}/{os.path.splitext(os.path.basename(model_txt_path))[0]}.pt"
 
         response = requests.get(link, stream=True)
 
         if response.status_code == 200:
             total_size = int(response.headers.get('content-length', 0))
     
-            progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True, desc=f"Downloading {filename}")
+            progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True, desc=f"Downloading {model_weights_path}")
             
-            with open(filename, 'wb') as file:
+            with open(model_weights_path, 'wb') as file:
                 for data in response.iter_content(chunk_size=1024):
                     size = file.write(data)
                     progress_bar.update(size)
             
             progress_bar.close()
-            print(f"Model downloaded successfully: {filename}")
+            print(f"Model downloaded successfully: {model_weights_path}")
         else:
             print(f"Model download failed. Status code: {response.status_code}")
 
     else:
         raise ValueError(f"Invalid Model Type: {model_type}")
     
-    return None
+    return model_weights_path
+
 
