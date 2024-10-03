8f797df5fa5fde81506ea05f20cdc93b2b589a6b
dylandru <158249525+dylandru@users.noreply.github.com>
2024-10-03 00:57:25 -0400

    Create LoadTools class with BDL API use

--- scripts/load_tools.py
+++ scripts/load_tools.py
@@ -4,103 +4,111 @@ from tqdm import tqdm
 import zipfile
 import io
 from scripts.function_utils import model_aliases, dataset_aliases
-from typing import Optional
+from typing import Optional, Union
 
-def load_model(model_alias: str, model_type = 'YOLO') -> str:
-    '''
-    Loads a given baseball computer vision model into the repository.
+class LoadTools:
+    """
+    Class dedicated to downloading models and datasets from either the BallDataLab API or specified text files.
 
-    Args:
-        model_alias (str): Alias of the model to load that corresponds to a model file to download
+    This class provides methods to download and load models and datasets
+    either from a specified URL or from the BallDataLab API. It supports
+    downloading both individual files (for models) and zip archives (for datasets).
 
-    Returns:
-        model_weights_path (str): Path to where the model weights are saved within the repo.
-    '''
+    Attributes:
+        session (requests.Session): A session object for making HTTP requests.
+        chunk_size (int): The size of chunks to use when downloading files.
+        BDL_MODEL_API (str): The base URL for the BallDataLab model API.
+        BDL_DATASET_API (str): The base URL for the BallDataLab dataset API.
 
-    if model_type == 'YOLO':
+    Methods:
+        load_model(model_alias: str, model_type: str = 'YOLO', use_bdl_api: Optional[bool] = True) -> str:
+            Loads a given baseball computer vision model into the repository.
+        load_dataset(dataset_alias: str, use_bdl_api: Optional[bool] = True) -> str:
+            Loads a zipped dataset and extracts it to a folder.
+        _download_files(url: str, dest: Union[str, os.PathLike], is_dataset: bool = False) -> None:
+            Protected method to handle model and dataset downloads.
+        _get_url(alias: str, txt_path: str, use_bdl_api: bool, api_endpoint: str) -> str:
+            Protected method to obtain the download URL from the BDL API or a text file.
+    """
 
-        model_txt_path = model_aliases.get(model_alias)
-        if not model_txt_path:
-            raise ValueError(f"This is not a model alias: {model_alias}")
+    def __init__(self):
+        self.session = requests.Session()
+        self.chunk_size = 1024
+        self.BDL_MODEL_API = "https://balldatalab.com/api/models/"
+        self.BDL_DATASET_API = "https://balldatalab.com/api/datasets/"
 
-        with open(model_txt_path, 'r') as file:
-            link = file.read().strip()
-
-        model_weights_path = f"{os.path.dirname(model_txt_path)}/{os.path.splitext(os.path.basename(model_txt_path))[0]}.pt"
-
-        if os.path.exists(model_weights_path):
-            print(f"Model found at {model_weights_path}")
-            return model_weights_path
-
-        response = requests.get(link, stream=True)
-
+    def _download_files(self, url: str, dest: Union[str, os.PathLike], is_dataset: bool = False) -> None:
+        response = self.session.get(url, stream=True)
         if response.status_code == 200:
             total_size = int(response.headers.get('content-length', 0))
-    
-            progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True, desc=f"Downloading {model_weights_path}")
+            progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True, desc=f"Downloading {os.path.basename(dest)}")
             
-            with open(model_weights_path, 'wb') as file:
-                for data in response.iter_content(chunk_size=1024):
-                    size = file.write(data)
+            if is_dataset:
+                content = io.BytesIO()
+                for data in response.iter_content(chunk_size=self.chunk_size):
+                    size = content.write(data)
                     progress_bar.update(size)
-            
-            progress_bar.close()
-            print(f"Model downloaded successfully: {model_weights_path}")
+                
+                progress_bar.close()
+                
+                with zipfile.ZipFile(content) as zip_ref:
+                    for file in zip_ref.namelist():
+                        if not file.startswith('__MACOSX') and not file.startswith('._'):
+                            zip_ref.extract(file, dest)
+                
+                print(f"Dataset downloaded and extracted to {dest}")
+            else:
+                with open(dest, 'wb') as file:
+                    for data in response.iter_content(chunk_size=self.chunk_size):
+                        size = file.write(data)
+                        progress_bar.update(size)
+                
+                progress_bar.close()
+                print(f"Model downloaded to {dest}")
         else:
-            print(f"Model download failed. Status code: {response.status_code}")
+            print(f"Download failed. STATUS: {response.status_code}")
 
-    else:
-        raise ValueError(f"Invalid Model Type: {model_type}")
-    
-    return model_weights_path
+    def _get_url(self, alias: str, txt_path: str, use_bdl_api: bool, api_endpoint: str) -> str:
+        if use_bdl_api:
+            return f"{api_endpoint}{alias}"
+        else:
+            with open(txt_path, 'r') as file:
+                return file.read().strip()
 
-def load_dataset(dataset_alias: str, file_txt_path: Optional[str] = None) -> str:
-    '''
-    Loads a zipped dataset from a .txt file containing a link to the dataset and extracts it to a folder.
+    def load_model(self, model_alias: str, model_type: str = 'YOLO', use_bdl_api: Optional[bool] = True) -> str:
+        if model_type == 'YOLO':
+            model_txt_path = model_aliases.get(model_alias)
+            if not model_txt_path:
+                raise ValueError(f"This is not a model alias: {model_alias}")
 
-    Args:
-        dataset_alias (str): Alias of the dataset to load that corresponds to a dataset folder to download
-        file_txt_path (Optional[str]): Path to .txt file containing download link to zip file containing dataset. 
+            model_weights_path = f"{os.path.dirname(model_txt_path)}/{os.path.splitext(os.path.basename(model_txt_path))[0]}.pt"
 
-    Returns:
-        dir_name (str): Path to the folder containing the dataset.
-    '''
+            if os.path.exists(model_weights_path):
+                print(f"Model found at {model_weights_path}")
+                return model_weights_path
 
-    if file_txt_path is None:
-        file_txt_path = dataset_aliases.get(dataset_alias)
+            url = self._get_url(model_alias, model_txt_path, use_bdl_api, self.BDL_MODEL_API)
+            self._download_files(url, model_weights_path)
+        else:
+            raise ValueError(f"Invalid Model Type... Only 'YOLO' is supported (right now).")
+        
+        return model_weights_path
+
+    def load_dataset(self, dataset_alias: str, file_txt_path: Optional[str] = None, use_bdl_api: Optional[bool] = True) -> str:
         if file_txt_path is None:
-            raise ValueError(f"This is not a dataset alias.")
+            file_txt_path = dataset_aliases.get(dataset_alias)
+            if file_txt_path is None:
+                raise ValueError(f"This is not a dataset alias.")
 
-    with open(file_txt_path, 'r') as file:
-        link = file.read().strip()
+        base = os.path.splitext(os.path.basename(file_txt_path))[0]
+        dir_name = "unlabeled_" + base if 'raw_photos' in base or 'frames' in base or 'frames' in dataset_alias else base
 
-    base = os.path.splitext(os.path.basename(file_txt_path))[0]
-    dir_name = f"unlabeled_{base}" if 'raw_photos' in base else base #add unlabeled_ to raw_photos datasets
+        if os.path.exists(dir_name):
+            print(f"Dataset found at {dir_name}")
+            return dir_name
 
-    if os.path.exists(dir_name):
-        print(f"Dataset found at {dir_name}")
-        return dir_name
-
-    response = requests.get(link, stream=True)
-    if response.status_code == 200:
-        total_size = int(response.headers.get('content-length', 0))
-        progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True, desc=f"Downloading {dir_name}")
-        
-        content = io.BytesIO()
-        for data in response.iter_content(1024):
-            size = content.write(data)
-            progress_bar.update(size)
-        progress_bar.close()
-
+        url = self._get_url(dataset_alias, file_txt_path, use_bdl_api, self.BDL_DATASET_API)
         os.makedirs(dir_name, exist_ok=True)
+        self._download_files(url, dir_name, is_dataset=True)
 
-        with zipfile.ZipFile(content) as zip_ref:
-            for file in zip_ref.namelist():
-                if not file.startswith('__MACOSX') and not file.startswith('._'): #prevents extracting MACOSX files from zip
-                    zip_ref.extract(file, dir_name)
-        
-        print(f"Dataset downloaded and extracted to {dir_name}.")
-    else:
-        print(f"Failed to download. Status code: {response.status_code}")
-
-    return dir_name
+        return dir_name
