7e7679d3d729f2cabce19768ce34faca1cf05b88
dylandru <158249525+dylandru@users.noreply.github.com>
2024-11-30 13:22:38 -0500

    Delete comment in _init_ file

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,4 +1,3 @@
-#from .generate_photos import generate_photo_dataset
 from .load_tools import LoadTools
 from .savant_scraper import BaseballSavVideoScraper
 from .function_utils.utils import extract_frames_from_video
