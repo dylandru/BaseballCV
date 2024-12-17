a514bbebd7fbe3c7dd8fa23d64323b0df3a495e3
dylandru <158249525+dylandru@users.noreply.github.com>
2024-12-17 12:54:49 -0500

    V1 of ModelVisualizationTools class for Model Classes

---
+++ scripts/model_classes/utils/model_visualization_tools.py
@@ -0,0 +1,6 @@
+class ModelVisualizationTools:
+    def __init__(self, model_name: str, model_run_path: str):
+        self.model_name = model_name
+        self.model_run_path = model_run_path
+
+    
