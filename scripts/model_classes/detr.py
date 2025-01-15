1b1beb5e887865a985b9d9f00f93b5a0dff0e102
dylandru <158249525+dylandru@users.noreply.github.com>
2025-01-15 12:09:31 -0500

    Altered Progress Callback to more strictly adhere to source with TQDM customization

--- scripts/model_classes/detr.py
+++ scripts/model_classes/detr.py
@@ -156,11 +156,11 @@ class DETR:
                     EarlyStoppingCallback(
                         early_stopping_patience=patience,
                         early_stopping_threshold=patience_threshold
-                    )
+                    ),
+                    CustomProgressBarCallback(num_epochs=epochs)
                 ]
             else:
-                callbacks = [
-                ]
+                callbacks = [CustomProgressBarCallback(num_epochs=epochs)]
 
             self.logger.info("Setting Up Trainer")
 
