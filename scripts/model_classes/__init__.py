7ed8c3b0bb051fa333fa76b959e8c75f7bdc6fea
dylandru <158249525+dylandru@users.noreply.github.com>
2025-02-12 19:48:26 -0500

    Remove scripts and src folders

--- scripts/model_classes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .florence2 import Florence2
-from .paligemma2 import PaliGemma2
-from .detr import DETR
-
-__all__ = ['Florence2', 'PaliGemma2', 'DETR']
--- scripts/model_classes/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .dataset_jsonl_detection import JSONLDetection
-from .model_function_utils import ModelFunctionUtils
-from .model_visualization_tools import ModelVisualizationTools
-from .model_logger import ModelLogger
-from .dataset_processor import DataProcessor
-from .dataset_coco_detection import CocoDetectionDataset
-
-__all__ = ['JSONLDetection', 'CocoDetectionDataset', 'ModelFunctionUtils', 'ModelVisualizationTools', 'ModelLogger', 'DataProcessor']
