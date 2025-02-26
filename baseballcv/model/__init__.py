b07ff91f56f69cb77ae7102ad75fb315cb2b04e3
dylandru <158249525+dylandru@users.noreply.github.com>
2025-02-26 13:08:37 -0500

    Fix __init__ file

--- baseballcv/model/__init__.py
+++ baseballcv/model/__init__.py
@@ -1,4 +1,4 @@
-from .od import DETR
+from .od import DETR, YOLOv9
 from .vlm import PaliGemma2, Florence2
 
-__all__ = ['DETR', 'PaliGemma2', 'Florence2']
+__all__ = ['DETR', 'PaliGemma2', 'Florence2', 'YOLOv9']
