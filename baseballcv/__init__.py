01b2eb83ff4a0f1dfcb982a4053acd3c6e7dcfc2
dylandru <158249525+dylandru@users.noreply.github.com>
2025-05-12 15:15:17 -0400

    Update Automatic Git Dependency Installer

--- baseballcv/__init__.py
+++ baseballcv/__init__.py
@@ -1,11 +1,14 @@
 """BaseballCV - A collection of tools and models designed to aid in the use of Computer Vision in baseball."""
 
-__version__ = "0.1.21"
+__version__ = "0.1.22"
 
 try:
-    from .utilities import check_and_install
-    check_and_install()
+    from .utilities.dependencies.git_dependency_installer import check_and_install_dependencies
+    check_and_install_dependencies()
 except Exception as e:
-    print(f"Error checking and installing git dependencies: {e}")
+    print(f"Warning: Failed to check/install git dependencies: {e}")
+    print("You may need to manually install dependencies using: from baseballcv")
+
+
 
 
