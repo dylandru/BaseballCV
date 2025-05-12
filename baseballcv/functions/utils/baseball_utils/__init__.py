5ea44753b33b9ae073c9858686c7915f2e055448
camarcano <c.marcano@gmail.com>
2025-05-12 11:00:37 -0600

    Added Command Analyzer use to the BaseballTools class.

--- baseballcv/functions/utils/baseball_utils/__init__.py
+++ baseballcv/functions/utils/baseball_utils/__init__.py
@@ -1,4 +1,5 @@
 from .distance_to_zone import DistanceToZone
 from .glove_tracker import GloveTracker
+from .command_analyzer import CommandAnalyzer # Add this line
 
-__all__ = ['DistanceToZone', 'GloveTracker']
+__all__ = ['DistanceToZone', 'GloveTracker', 'CommandAnalyzer'] # Add CommandAnalyzer here
