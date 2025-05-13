73663b00a537bfa95ee20a36c3d897a50f39e465
camarcano <c.marcano@gmail.com>
2025-05-13 12:06:34 -0600

    Skeleton code for the EventDetector class.

--- baseballcv/functions/utils/baseball_utils/__init__.py
+++ baseballcv/functions/utils/baseball_utils/__init__.py
@@ -1,5 +1,7 @@
+# In baseballcv/functions/utils/baseball_utils/__init__.py
 from .distance_to_zone import DistanceToZone
 from .glove_tracker import GloveTracker
-from .command_analyzer import CommandAnalyzer # Add this line
+from .command_analyzer import CommandAnalyzer
+from .event_detector import EventDetector # Make sure this line is present
 
-__all__ = ['DistanceToZone', 'GloveTracker', 'CommandAnalyzer'] # Add CommandAnalyzer here
+__all__ = ['DistanceToZone', 'GloveTracker', 'CommandAnalyzer', 'EventDetector'] # And EventDetector is here
